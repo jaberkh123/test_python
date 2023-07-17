fc8b91f47ad3db61c56f0444d7b753e07753f318
Jaber Khorramshahi <96335504+jaberkh123@users.noreply.github.com>
2023-07-17 06:58:12 +0330

    Update db.py

--- db.py
+++ db.py
@@ -1,22 +1,49 @@
-class Reading:
-    # TODO: change this to represent whatever information is needed
-    pass
+import mysql.connector
+from mysql.connector import Error
+from datetime import datetime
+
+def to_date(timestamp):
+     return datetime.fromtimestamp(int(timestamp))
 
 
-# This is a fake database which stores data in-memory while the process is running
-# Feel free to change the data structure to anything else you would like
-database: dict[str, Reading] = {}
+def get_data_by_date(start_date, end_date):
+    try:
+        connection = mysql.connector.connect(user='user', password='pass', host='localhost', port=3306, database='data_baes')
+        mycursor = connection.cursor()
+
+        sql = "SELECT * FROM data2 WHERE query_date BETWEEN %s AND %s"
+        val = (start_date, end_date)
+        mycursor.execute(sql, val)
+        records = mycursor.fetchall()
+
+        mycursor.close()
+        connection.close()
+
+        return records
+
+    except Exception as e:
+        print(f"Error while fetching data from database: {e}")
+        return False
+
+def add_db(triples):
+    try:
+        connection = mysql.connector.connect(user='user', password='pass', host='localhost', port=3306, database='data_baes')
+        mycursor = connection.cursor()
+
+        sql = "INSERT INTO data (query_date, name, num) VALUES (%s, %s, %s)"
+        val = [(to_date(triple[0]), triple[1], triple[2]) for triple in triples]
+        mycursor.executemany(sql, val)
+        connection.commit()
+
+        mycursor.close()
+        connection.close()
+
+        print('Connection to MySQL database closed')
+        return True
+
+    except Exception as e:
+        print(f"Error while inserting data into database: {e}")
+        return False
 
 
-def add_reading(key: str, reading: Reading) -> None:
-    """
-    Store a reading in the database using the given key
-    """
-    database[key] = reading
 
-
-def get_reading(key: str) -> Reading | None:
-    """
-    Retrieve a reading from the database using the given key
-    """
-    return database.get(key)
